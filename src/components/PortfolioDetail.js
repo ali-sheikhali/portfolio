0b292f1abbf5c92f6f5d56cd625a389a71f06c8b
ali-sheikhali <ali.sheikhali1998@gmail.com>
2024-04-14 21:43:45 +0330

    add file

--- src/components/PortfolioDetail.js
+++ src/components/PortfolioDetail.js
@@ -2,10 +2,24 @@ import React from "react";
 import { Link, useParams } from "react-router-dom";
 import WideScreenNavBar from "./navbar/WideScreenNavBar";
 import MobileBar from "./navbar/MobileBar";
+import tmdb from "../media/tmdblogo.png";
+import learning from "../media/learning.png";
+import weather from "../media/weather.png";
+import shop from "../media/shop.png";
 function PortfolioDetail() {
   const { id } = useParams();
+  let img;
+  if (id === "1") {
+    img = tmdb;
+  } else if (id === "2") {
+    img = learning;
+  } else if (id === "3") {
+    img = weather;
+  } else {
+    img = shop;
+  }
   return (
-    <div >
+    <div>
       <div className="lg:hidden w-11/12 mx-auto my-5">
         <div>
           <MobileBar />
@@ -14,6 +28,8 @@ function PortfolioDetail() {
           <div className="border my-10">
             <h2>Portfolio Detail</h2>
             <p>ID: {id}</p>
+            <img src={img} alt="" />
+
           </div>
         </div>
       </div>
@@ -26,6 +42,7 @@ function PortfolioDetail() {
         <div className="w-9/12 border my-5 rounded-lg">
           <h2>Portfolio Detail</h2>
           <p>ID: {id}</p>
+          <img src={img} alt="" />
         </div>
       </div>
     </div>
